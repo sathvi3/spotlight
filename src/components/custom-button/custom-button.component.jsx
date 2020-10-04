f31601aecd37738268aa5e14571373ad8ed86529
sathvi3 <sathvikbhandar007@hotmail.com>
2020-10-04 17:52:50 +0530

    fixed bugs

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -8,7 +8,9 @@ const CustomButton = ({
   ...otherProps
 }) => (
   <button
-    className={`${inverted ? "inverted" : ""} custom-button`}
+    className={`${inverted ? "inverted" : ""} ${
+      isGoogleSignIn ? "google-sign-in" : ""
+    } custom-button`}
     {...otherProps}
   >
     {children}
